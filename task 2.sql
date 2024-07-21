be9157344a8bd8fa2b3564e7f8372809fc9af9c0
Arshiya <139642183+arshiyaaanaaz@users.noreply.github.com>
2024-07-22 00:39:56 +0530

    Add files via upload

---
+++ task 2.sql
@@ -0,0 +1,64 @@
+CREATE TABLE student_info
+(
+	Student_id serial primary key,
+	Name varchar,
+	Age int,
+	Gender varchar,
+	Course varchar,
+	Batch_time int 
+
+	)
+	SELECT * FROM student_info
+INSERT INTO student_info (Name, Age,Gender,Course,Batch_time)
+values('aly',20,'female','DATA ANALYSIS' ,8),('mark',23,'male','DATA SCIENCES',7),
+	('Den',24,'male','WEB DEVELOPMENT',9),('Rosy',19,'female','DATA SCIENCES',7),
+('honey',22,'female','DATA ANALYSIS',8),('Rony',23,'male','WEB DEVELOPMENT',9),
+('Noman',25,'male','DATA SCIENCES',7),('July',21,'female','DATA ANALYSIS',8),
+('Luna',24,'female','DATA SCIENCES',7),('Emily',23,'female','WEB DEVELOPMENT',9),
+('luke',30,'male','WEB DEVELOPMENT',9),('Ruby',25,'female','DATA ANALYSIS',8),('Bella',18,'female','DS',7),
+('camilla',22,'female','DA',8),('adam',22,'male','DS',7),('heryy',19,'male','DS',7),
+('money',25,'male','WD',9),('eva',23,'female','DA',8),('justin',26,'male','WD',9),('david',20,'male','DS',7)
+
+	SELECT * FROM student_info
+INSERT INTO student_info (Name, Age,Gender,Course,Batch_time)
+values ('lily',21,'female','DATA ANALYSIS' ,8),
+('john',22,'male','DATA ANALYSIS' ,8),
+('jack',30,'male','DATA ANALYSIS' ,8),
+('Ben',20,'male','DATA ANALYSIS' ,8),
+('Anna',23,'female','DATA ANALYSIS' ,8),
+('Henry',24,'male','WEB DEVELOPMENT',9),
+('bella',25,'female','WEB DEVELOPMENT',9),
+('zoya',24,'male','WEB DEVELOPMENT',9)
+
+	SELECT * FROM student_info
+
+INSERT INTO student_info (Name, Age,Gender,Course,Batch_time)
+values
+('mith',33,'male','DATA ANALYSIS',8),
+('smith',23,'male','DATA SCIENCES',7),
+('liba',18,'female','DATA SCIENCES',7),
+('mona',19,'female','DATA SCIENCES',7),
+('zoe',22,'female','DATA ANALYSIS',8),
+('trunk',21,'male','DATA SCIENCES',7),
+('rumi',26,'male','WEB DEVLOPMENT',9),
+('Asra',23,'female','DATA SCIENCES',7),
+('Zam',27,'male','DATA ANALYSIS',8),
+('Luv',22,'male','WEB DEVLOPMENT',9),
+('Anna',22,'female','DATA SCIENCES',7),
+('Emma',25,'female','DATA ANALYSIS',8),
+('Yizzy',23,'female','DATA SCIENCES',7),
+('Sam',20,'female','WEB DEVLOPMENT',9),
+('Qais',22,'female','WEB DEVLOPMENT',9),
+('Luv',22,'male','WEB DEVLOPMENT',9),
+('misr',26,'male','WEB DEVLOPMENT',9),
+('ima',22,'female','WEB DEVLOPMENT',9),
+('Kim',22,'male','DATA ANALYSIS',8)
+
+
+	SELECT * FROM student_info
+
+
+
+
+
+
